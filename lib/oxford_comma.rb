1acc6e3ef62eaa1ba6c2fbbd4f406dddc9a81141
Kelly Dobbins <kellydobbins26@outlook.com>
2019-05-28 06:39:12 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,20 +1,4 @@
 def oxford_comma(array)
-array = ["kiwi"].join("")
-end
-
-def oxford_comma(array)
-array = ["kiwi", "durian"].insert(1, "and")
-array.join(,)
-end
-
-def oxford_comma(array)
-array = ["kiwi", "durian","starfruit"].insert(2, "and")
-end 
-
-def oxford_comma
-array = ["kiwi","durian","starfruit","mangos","dragon fruits"].insert(4, "and")
-end
-
-def oxford_comma(array)
-array = ["kiwi", "durian", "starfruit", "mangos", "dragon fruits", "lychees", "pomelos"].insert(6, "and")
-end
+  
+  if array.length == 2 
+    return "#{arra"
